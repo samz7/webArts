562164fa708a3ee6bd21d4c716ce1abdb949de13
samz7 <saminc97@gmail.com>
2020-03-12 22:18:31 +0600

    deleted some files

--- snake/Snake.js
+++ snake/Snake.js
@@ -1,103 +1,108 @@
-class Scales{
-    constructor(x, y){
+//Second Attempt with linked lists
+class Scale {
+    constructor(x, y, axis, direction) {
         this.x = x;
         this.y = y;
-        this.pos = [this.x, this.y];
-        this.axis = 0
-        this.direction = 1;
-        this.turns = []
-    }
-
-    move(){
-        this.change_direction();
-        this.pos[this.axis] += this.direction*1
-
-    }
-
-    change_direction(){
-
-
-        this.turns.forEach((turn)=>{
-            let turningPoint = turn[0];
-            let axis = turn[1];
-            let direction = turn[2];
-
-
-            if (this.direction < 0){
-                if (this.pos[this.axis] < turningPoint){
-                    
-                    this.axis = axis;
-                    this.direction = direction;
-                    }
-                    
-                }
-    
-    
-            else if (this.direction > 0){
-                if (this.pos[this.axis] > turningPoint){
-                    
+        this.pos = [this.x, this.y]
+        this.axis = axis;
+        this.direction = direction;
+        this.prev = null
         
-                    this.axis = axis;
-                    this.direction = direction;
-                        
-                    }
-                        
-                }
-             
-            
-        })
-    
-        this.turns = []
-    }
-        
-    
-
-    addTurn(turningPoint, axis, direction){
-
-        this.turns.push([turningPoint, axis, direction])
 
     }
 
-    
+    change_direction(axis, direction) {
 
-    render(){
-        rect(this.pos[0], this.pos[1], 20,20);
+        this.axis = axis;
+        this.direction = direction;
+    }
+
+    move() {
+        if (this.prev) {
+
+            this.pos = this.prev.pos.slice(0);
+            return 0;
+
+        }
+
+        this.pos[this.axis] += this.direction * 20
+        return this.pos;
+    }
+
+    render() {
+        fill(255)
+        rect(this.pos[0], this.pos[1], 20, 20);
     }
 }
 
+class Snake {
+    constructor(x, y) {
+        this.tail = new Scale(x, y, 0, 1);
+        this.headPos = this.tail.pos
+        this.axis = this.tail.axis;
+        this.len = 1;
+    }
 
+    run() {
 
-class Snake{
-    constructor(){
-        this.body = []
-        for (let i = 1; i < 4; i++){
-            this.body.push(new Scales(i*20, 10))
+        let current_scale = this.tail;
+        while (current_scale) {
+            if (!current_scale.prev) {
+                this.headPos = current_scale.move();
+            }
+            else {
+                current_scale.move();
+
+            }
+            current_scale = current_scale.prev;
         }
-        this.axis = 0;
-        this.turningPoint;
-        this.direction = 1;
-    }
 
-    change_direction(axis, direction){
-        this.axis = axis;
-        this.direction = direction;
-        let lastElement = this.body[this.body.length - 1]
-        this.turningPoint = lastElement.pos[lastElement.axis]
+        current_scale = this.tail;
+        while (current_scale) {
+            if (current_scale.prev) {
+                if (current_scale.pos[0] === this.headPos[0] && current_scale.pos[1] === this.headPos[1]) {
+                    console.log('you lose')
+                    return 1;
+                }
+            }
+            current_scale = current_scale.prev;
+        }
+
+
         
+
+        return 0;
+
     }
 
+    change_direction(axis, direction) {
 
-    run(){
-        this.body.forEach((scale)=>{
-            scale.addTurn(this.turningPoint, this.axis, this.direction)
+        if (axis != this.axis) {
+            this.axis = axis;
+            let current_scale = this.tail;
+            while (current_scale) {
 
-            scale.move();
-        })
+                current_scale.change_direction(axis, direction);
+                current_scale = current_scale.prev;
+            }
+        }
+
     }
 
-    render(){
-        this.body.forEach((scale)=>{
-            scale.render();
-        })
+    grow() {
+        //Problem is here
+        let prev = this.tail;
+        this.tail = new Scale(prev.pos[0] - 20 * prev.direction * (1 - prev.axis), prev.pos[1] - 20 * prev.direction * !(1 - prev.axis), prev.axis, prev.direction)
+        this.tail.prev = prev;
+        this.len += 1;
+
+    }
+
+    render() {
+        let current_scale = this.tail
+        while (current_scale) {
+            current_scale.render()
+            current_scale = current_scale.prev
+        }
     }
 }
